5e6b8dcd6aee9bd61875b736ce9e0f5bda850964
monstercr <przemyslaw.pawlowski.pp@gmail.com>
2020-04-25 22:19:58 +0200

    refactor before features

--- src/modules/users/saga.ts
+++ src/modules/users/saga.ts
@@ -1,15 +1,9 @@
-import { all, call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
 import { ApiConfig, Config } from '../../config';
-import { IUser } from '../../types/user';
 import Api from '../../utils/api';
 import * as actions from './actions';
 import { ActionTypes, IUsersRequest, IUsersRequestPayload } from './types/actions';
-
-const fetchRepos = async (payload: string): Promise<any> => {
-  const { data } = await Api.get(payload);
-  return data;
-};
 
 const fetchUsersApi = async (payload: IUsersRequestPayload): Promise<any> => {
   const { key } = payload;
@@ -23,13 +17,6 @@ export function* fetchUsersSaga(action: IUsersRequest) {
 
   try {
     const { ...usersData } = yield call(fetchUsersApi, payload);
-    const { ...reposData } = yield all(
-      usersData.items.map((item: any, key: number) => call(fetchRepos, item.repos_url))
-    );
-
-    usersData.items.forEach((user: IUser, key: number) => {
-      user.repositories = reposData[key];
-    });
 
     yield put(actions.userRequestSuccess(usersData.items));
   } catch (error) {
